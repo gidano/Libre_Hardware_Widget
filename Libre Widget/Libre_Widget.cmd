da423feea29019358a8edc51c54803f0b17861c0
gidano <gidano@gmail.com>
2026-09-17 11:45:52 +0200

    Update executable path in Libre_Widget.cmd

--- Libre Widget/Libre_Widget.cmd
+++ Libre Widget/Libre_Widget.cmd
@@ -1,5 +1,5 @@
 @echo off
-start "" "D:\Portable\libre_widget\Libre Widget v0.1.6.exe"
+start "" "X:\Portable\libre_widget\Libre Widget v0.1.6.exe"
 
 :: Add meg a helyes útvonalat!
 :: Enter the correct route!
